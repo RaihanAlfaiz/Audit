86b97c1f3e5d9370644943eccbe67e056ee50906
RaihanAlfaiz <raihanalfaiz80@gmail.com>
2024-06-13 09:52:37 +0700

    fix landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,14 +18,14 @@
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="landingpage/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="landingpage/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="landingpage/aos/aos.css" rel="stylesheet">
+  <link href="landingpage/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="landingpage/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Main CSS File -->
-  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="landingpage/main.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Arsha
@@ -602,17 +602,17 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="landingpage/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="landingpage/php-email-form/validate.js"></script>
+  <script src="landingpage/aos/aos.js"></script>
+  <script src="landingpage/glightbox/js/glightbox.min.js"></script>
+  <script src="landingpage/swiper/swiper-bundle.min.js"></script>
+  <script src="landingpage/waypoints/noframework.waypoints.js"></script>
+  <script src="landingpage/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="landingpage/isotope-layout/isotope.pkgd.min.js"></script>
 
   <!-- Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="landingpage/main.js"></script>
 
 </body>
 
