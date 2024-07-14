78b2c6301dfc0e1fd29202b45709a9bfa778bb83
RaihanAlfaiz <raihanalfaiz80@gmail.com>
2024-07-15 00:40:36 +0700

    add userrole in db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PackageSeeder::class,
             AdminSeeder::class,
+            UserRoleSeeder::class,
 
         ]);
     }
