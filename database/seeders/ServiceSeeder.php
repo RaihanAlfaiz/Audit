ba7fc115a9bbc7914c3a06717f5ed4dd2373dceb
RaihanAlfaiz <raihanalfaiz80@gmail.com>
2024-07-15 00:50:24 +0700

    add service seeder

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +14,21 @@ class ServiceSeeder extends Seeder
     public function run(): void
     {
         //
+        $data = [
+            ["id" => "1", "item" => "Ruangan Lecture Theatre", "unit" => "1", "unit_name" => "Ruangan", "price" => "3750000.00"],
+            ["id" => "2", "item" => "Proyektor Paket 3x4 M 5000 Lumen", "unit" => "2", "unit_name" => "Set", "price" => "1750000.00"],
+            ["id" => "3", "item" => "Kursi Merah", "unit" => "1", "unit_name" => "Buah", "price" => "8000.00"],
+            ["id" => "3", "item" => "Kursi Merah", "unit" => "1", "unit_name" => "Buah", "price" => "8000.00"],
+         
+        ];
+
+        foreach ($data as $x) {
+            if (!Service::where('id', $x['id'])->first()) {
+                $m = new Service();
+                $m->id = $x['id'];
+                $m->name = $x['name'];
+                $m->save();
+            }
+        }
     }
 }
