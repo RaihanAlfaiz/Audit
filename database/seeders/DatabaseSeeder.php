86c89145daa9a1c042eec0ac227f84e4e401059e
RaihanAlfaiz <raihanalfaiz80@gmail.com>
2024-07-15 00:42:41 +0700

    add role seeder in dbseed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Http\Middleware\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +20,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PackageSeeder::class,
             AdminSeeder::class,
+            RoleSeeder::class,
             UserRoleSeeder::class,
 
         ]);
