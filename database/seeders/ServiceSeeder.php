1401a60972dfd1423da434a5f6424c0bc4cf81d5
RaihanAlfaiz <raihanalfaiz80@gmail.com>
2024-07-15 00:52:18 +0700

    add service seeder

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -18,7 +18,9 @@ class ServiceSeeder extends Seeder
             ["id" => "1", "item" => "Ruangan Lecture Theatre", "unit" => "1", "unit_name" => "Ruangan", "price" => "3750000.00"],
             ["id" => "2", "item" => "Proyektor Paket 3x4 M 5000 Lumen", "unit" => "2", "unit_name" => "Set", "price" => "1750000.00"],
             ["id" => "3", "item" => "Kursi Merah", "unit" => "1", "unit_name" => "Buah", "price" => "8000.00"],
-            ["id" => "3", "item" => "Kursi Merah", "unit" => "1", "unit_name" => "Buah", "price" => "8000.00"],
+            ["id" => "4", "item" => "Panggung Portable (3x2M)", "unit" => "1", "unit_name" => "Buah", "price" => "50000.00"],
+            ["id" => "5", "item" => "Panggung Portable (1,5x2M)", "unit" => "1", "unit_name" => "Buah", "price" => "20000.00"],
+            ["id" => "6", "item" => "Partisi Merah", "unit" => "1", "unit_name" => "Buah", "price" => "10000.00"],
          
         ];
 
