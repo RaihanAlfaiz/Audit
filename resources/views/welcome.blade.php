e9e181343cfcb233bcc98026e71e3d29432a7fb3
sardeliw <153065487+sardeliw@users.noreply.github.com>
2024-07-20 16:31:57 +0700

    style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -576,7 +576,7 @@
                 .social-link {
                     display: flex;
                     align-items: center;
-                    margin-bottom: 10px; /* Atur jarak antar icon jika diperlukan */
+                    margin-bottom: 10px; /* untuk mengatrur jarak antar icon jika diperlukan */
                 }
 
                 .social-link a {
