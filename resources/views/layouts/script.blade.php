918e2a650ffaeb89c13105a93e9a52df79cf0604
RaihanAlfaiz <raihanalfaiz80@gmail.com>
2024-07-25 20:47:45 +0700

    fix datatable

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -23,9 +23,10 @@
 <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-<script
+{{-- <script
 src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js">
-</script>
+</script> --}}
+<script src="{{asset('assets/js/datatable-sneat.js')}}"></script>
 
 <!-- Page JS -->
 @yield('script')
